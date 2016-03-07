267ab755b36f48155f9700fcbea7cced8e0894b9
afei <88603982@qq.com>
2016-03-07 17:57:41 +0800

    update wiget

---
+++ dist/widget-mask.js
@@ -0,0 +1,1 @@
+!function(i,e){i.component("widget-mask",{props:["visible"],template:'<div class="widget-mask" ></div>'})}(Vue,window);
--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,26 @@
   var gulp = require('gulp');
   var del = require('del');
   var $ = require('gulp-load-plugins')();
+
   gulp.task('clean', function () {
     return del('dist');
   });
 
+  gulp.task('sass', ['clean'], function () {
+    return gulp.src('src/widget-mask.scss')
+      .pipe($.sass({
+        outputStyle: 'compressed'  //`nested`, `expanded`, `compact`, `compressed`
+      }))
+      .pipe(gulp.dest('dist'));
+  });
 
-  gulp.task('default',['clean'])
+  gulp.task('minifyJs',['clean'],function(){
+    return gulp.src('src/widget-mask.js')
+      .pipe($.uglify())
+      .pipe(gulp.dest('dist'));
+  });
+
+
+  gulp.task('default', ['minifyJs','sass'])
 
 }());
